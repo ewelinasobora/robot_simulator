678671251f3b21f7f74f8bfd166f037f6e1218bd
Ewelina Sobora <e@e-dot.uk>
2024-09-23 14:08:18 +0100

    ensures robot coordinates placement is not negative

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,9 +1,25 @@
 class Robot
-  attr_accessor :x, :y, :facing
+  DIRECTIONS = %w(NORTH EAST SOUTH WEST)
+
+  attr_accessor :x_coordinate, :y_coordinate, :facing
 
   def initialize(x_coordinate: nil, y_coordinate: nil, facing: nil)
-    @x = x_coordinate
-    @y = y_coordinate
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
     @facing = facing
+  end
+
+  def place(x, y, f)
+    return unless valid_position?(x, y) && DIRECTIONS.include?(f)
+
+    @x_coordinate = x
+    @y_coordinate = y
+    @facing = f
+  end
+
+  private
+
+  def valid_position?(x, y)
+    x >= 0 && y >= 0
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,4 +2,17 @@ require_relative '../lib/robot'
 
 RSpec.describe Robot do
   subject { described_class.new() }
+
+  describe '#place' do
+    it 'places the robot at the specified position and direction' do
+      subject.place(1, 2, 'NORTH')
+      expect(subject.x_coordinate).to eq(1)
+      expect(subject.y_coordinate).to eq(2)
+      expect(subject.facing).to eq('NORTH')
+    end
+
+    it 'returns false when placement is invalid' do
+      expect(subject.place(-1, 2, 'NORTH')).to be_falsey
+    end
+  end
 end
