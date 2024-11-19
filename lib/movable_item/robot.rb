84a4506e94b459f3b846af6a3015d434e27f2b20
Ewelina Sobora <11948354+ewelinasobora@users.noreply.github.com>
2024-11-19 14:52:14 +0000

    Update robot.rb

--- lib/movable_item/robot.rb
+++ lib/movable_item/robot.rb
@@ -3,7 +3,6 @@
 require_relative 'movable_item'
 # Represents a robot that can move and face different directions.
 class Robot < MovableItem
-  # DIRECTIONS = %w[NORTH EAST SOUTH WEST].freeze
 
   def initialize(x_coordinate: nil, y_coordinate: nil, facing: nil, steps: 1)
     super(x_coordinate:, y_coordinate:, facing:)
